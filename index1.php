e15ee043ef253ae031a464c3a9d144dd6f513fce
“viktorcvp” <dffggg2013@yandex.by>
2023-01-04 20:01:28 +0300

    index1

---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php
+const QUISTION1 = "какая задача стоит перед вами сегодня? ";
+$timeQuestion = "Сколько примерно времени это займет? ";
+
+$name = readline("Привет, как вас зовут? :");
+$ege = (int)readline("сколько вам лет? :");
+
+echo "Вас зовут $name, вам $ege лет\n";
+
+$task = readline(QUISTION1);
+$time = (int)readline($timeQuestion);
+$task1 = readline(QUISTION1);
+$time1 = (int)readline($timeQuestion);
+$task2 = readline(QUISTION1);
+$time2 = (int)readline($timeQuestion);
+$sum = $time + $time1 + $time2;
+
+echo "$name, сегодня у вас запланировано 3 приоритетных задачи на день:
+$task $time(ч)
+$task1 $time1(ч)
+$task2 $time2(ч)\n";
+
+echo "Примерное время выполнения плана = {$sum}ч";
